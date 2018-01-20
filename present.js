80c55ad81ff3550e250f35903e9f9448d4846a8e
Daniel Patru <dpatru@gmail.com>
2018-01-20 15:06:15 -0500

    Add White on Black. Show fontsize.

--- present.js
+++ present.js
@@ -10,8 +10,13 @@ function escapeHtml(unsafe) {
 }
 var emphPattern = /(\W)\*(\w.*\S)\*(\W)/.compile()
 function emphasize(txt) {
-    return txt.replace(/(\W)\*(\w.*?\S)\*(\W)/g,
-		       function(m,m1,m2,m3){return m1+"<em>"+m2+"</em>"+m3;});
+    return txt
+	.replace(/(\W)\*\*(\w.*?\S)\*\*(\W)/g,
+		 function(m,m1,m2,m3){
+		     return m1+'<em class="bold">'+m2+'</em>'+m3;})
+	.replace(/(\W)\*(\w.*?\S)\*(\W)/g,
+		 function(m,m1,m2,m3){
+		     return m1+'<em class="hilite">'+m2+'</em>'+m3;});
 }
 function loadFile() {
     // https://stackoverflow.com/a/33641308/268040
@@ -28,8 +33,8 @@ function updateDisplay(){
     console.log('updating display this = ', this);
     display.document.body.innerHTML =
 	"<style>"+
-	"em {background:yellow}"+
-	"body {white-space:pre-wrap}"+
+	document.querySelector('input[name="style"]:checked').value+
+	"body {margin:1em; white-space:pre-wrap;}"+
 	"</style>"+
 	emphasize(escapeHtml($("content").value));
 }
@@ -39,14 +44,18 @@ document.body.innerHTML +=
     '<input id="myFile" type="file" onchange="loadFile()"/>' + 
     '<textarea id="content" style="display:block"></textArea>' + 
     '<button onclick="updateDisplay();">Update Display</button>' +
-    '<br><label>fontSize<input type="range" min="1" max="10" step=".1" value="1" oninput="updateFontSize(this.value)" onchange="updateFontSize(this.value)"></label>';
+    '<br><label>fontSize <span id="fontSize">1 em</span> <input type="range" min="1" max="10" step=".1" value="1" oninput="updateFontSize(this.value)" onchange="updateFontSize(this.value)"></label>' +
+    '<br><label><input name="style" checked type="radio" value="em.hilite {font-style:normal; background:yellow} em.bold {font-style:normal; color:red}"> black on white</label>'+
+    '<br><label><input name="style" type="radio" value="em.hilite {font-style: normal; color:yellow} em.bold {font-style:normal;color:lightcoral} body {color:white; background-color:black}"> white on black</label>'+
+    '';
+
     // '<textarea id="content" style="display:block; width:100%"></textarea>'+
     // '<button onclick="updateDisplay();">Update Display</button>';
 
 function updateFontSize(s){
     console.log('changing font size to '+s);
     var b = display.document.body;
-    b.style.fontSize = s+'em';
+    b.style.fontSize = $('fontSize').innerText = s+'em';
 }
 
 function maximize() {
